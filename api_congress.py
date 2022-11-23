63d5533d4aefaff7391bdeaf17919273ad5e9486
Julian <julian.kath@posteo.de>
2022-11-23 16:47:42 +0100

    updates_script

--- api_congress.py
+++ api_congress.py
@@ -24,17 +24,15 @@ if __name__ == '__main__':
                                         req.get_method = lambda: 'GET'
                                         response = urllib.request.urlopen(req, timeout=5)
                                         
-                                        output = response.read()
-                                        
                                         if output_format == 'json':
+                                                
+                                                output = response.read()
                                                 
                                                 output = json.loads(output)
                                                 
                                         if output_format == 'html':
                                                 
-                                                root = ET.ElementTree(ET.fromstring(output)).getroot()
-                                                
-                                                output = [node.text for node in root.findall('.//pre')]
+                                                output = response.read()
                                         
                                         return output
                                 
@@ -48,7 +46,7 @@ if __name__ == '__main__':
         
         
         @congress_deco(output_format='json')
-        def get_congress_data(query:str, api_key:str, *args) -> dict:
+        def get_congress_data(query:str, *args, api_key=os.getenv('US.GOV_API')) -> dict:
                 
                 hdr = {
                         # specifying requested encoding
@@ -64,7 +62,7 @@ if __name__ == '__main__':
                 query_url = BASE_URL + query
                 
                 if args: # concatenate variable arguments to url
-                                query_url += '&' + '&'.join(args)
+                        query_url += '&' + '&'.join(args)
                                 
                 return (query_url, hdr)
         
@@ -137,9 +135,15 @@ if __name__ == '__main__':
         def get_root_url(url):
                 return url.replace('https://api.congress.gov/v3/', '').replace('?format=json', '')
         
-        output_l = []
         
-        bills_recent = bills_df[bills_df.congress > 115]
+        bills_df = pd.read_csv('data/bills2.csv')
+        
+        output_l = []
+        list_cosponsors = []
+        text_list = []
+        summary_list = []
+        
+        bills_recent = bills_df[bills_df['congress'].isin([116, 117])].reset_index()
         
         counter = 0
         start_t = time.time()
@@ -147,11 +151,15 @@ if __name__ == '__main__':
         api_keys = list(map(os.getenv, ['US.GOV_API', 'US.GOV_API2', 'US.GOV_API3', 'US.GOV_API4']))
         api_key = api_keys.pop()
         
-        for _, row in bills_recent.reset_index(inplace=True).iterrows():
+        bills_recent = bills_recent.iloc[116:130,:]
+              
+        for i, row in bills_recent.iterrows():
                 
-                counter += 7
+                print(f'Querying bill {i} of {len(bills_recent)}')
                 
-                if (counter%4000 <= 7) & (counter > 10):
+                counter += 6
+                
+                if (counter%4000 <= 6) & (counter > 10):
                         
                         end_t = time.time()
                         elapsed = end_t - start_t
@@ -159,47 +167,95 @@ if __name__ == '__main__':
                         
                         start_t = time.time()
                         
-                        counter = 7
-                
-                elif (counter%1000 <= 7) & (counter > 10):
+                        counter = 6
+                        
+                elif (counter%1000 <= 6) & (counter > 10):
                         
                         if api_keys:
                                 api_key = api_keys.pop()
                         else:
                                 api_keys = list(map(os.getenv, ['US.GOV_API', 'US.GOV_API2', 'US.GOV_API3', 'US.GOV_API4']))
-                        
+                
                 try:
-                        metadata = get_congress_data(get_root_url(row['url']))
+                        metadata = get_congress_data(get_root_url(row['url']), api_key=api_key)
                         # TODO: get infos from metadata dictionary, like sponsors, originChamber, polyArea, title
                         
-                        text_url = get_congress_data(
-                                get_root_url(row['url']) + '/text')['textVersions'][0]['formats'][0]['url']
+                        bill_number = metadata['bill']['number']
+                        policy_area = metadata['bill']['policyArea']['name']
+                        if 'cosponsors' in metadata['bill'].keys():
+                                cosponsors = get_congress_data(get_root_url(metadata['bill']['cosponsors']['url']) + "?limit=250", api_key=api_key)
+                                cosponsor_D = 0
+                                cosponsor_R = 0
+                                cosponsor_l = []
+                                for cosponsor in cosponsors['cosponsors']:
+                                        cosponsor_party = cosponsor['party']
+                                        cosponsor_D += 1 if cosponsor_party == 'D' else 0
+                                        cosponsor_R += 1 if cosponsor_party == 'R' else 0
+                                        cosponsor_name = cosponsor['firstName'] + ' ' + cosponsor['lastName']
+                                        cosponsor_party = cosponsor['party']
+                                        cosponsor_l.append({'cosponsor_name': cosponsor_name, 'cosponsor_party': cosponsor_party, 'number': bill_number})
+                                #number = int(output_l[0]['metadata']['bill']['number'])
+                                cosponsor_D = cosponsor_D/(cosponsor_D + cosponsor_R)
+                                cosponsor_R = cosponsor_R/(cosponsor_D + cosponsor_R)
+                        else:
+                                cosponsor_D = 0
+                                cosponsor_R = 0
+                                cosponsor_l = []
+                                
+                        latest_action = metadata['bill']['latestAction']['actionDate']
                         
-                        text = get_bill_text(text_url)
+                        try:
+                                
+                                text_l = get_congress_data(get_root_url(row['url']) + '/text', api_key=api_key)['textVersions']
+                                
+                                if len(text_l) > 2:
+                                        
+                                        text_dict = {i['type']: get_bill_text(i['formats'][0]['url']) for i in text_l if (i['formats'] and i['type'])}
+                                else:
+                                        text_dict = {text_l[0]['type']: get_bill_text(text_l[0]['formats'][0]['url'])}
+                                
+                                text_dict['bill'] = row['number']
+                                text_dict['title'] = row['title']
                         
-                        summary = get_congress_data(
-                                get_root_url(row['url']) + '/summaries')['summaries'][0]['text']
+                        except Exception as er:
+                                
+                                print(er)
+                                text_dict = None
                         
-                        subjects = get_congress_data(
-                                get_root_url(row['url']) + '/subjects')['subjects']
+                        summary_l = get_congress_data(get_root_url(row['url']) + '/summaries', api_key=api_key)['summaries']
                         
-                        related_bills = get_congress_data(
-                                get_root_url(row['url']) + '/relatedbills')
+                        summary_dict = {i['actionDesc']: i['text'] for i in summary_l}
                         
-                        dic_i = {'metadata': metadata,
-                        'text' : text,
-                        'summary': summary,
-                        'subjects': subjects,
-                        'related_bills': related_bills
-                        }
+                        summary_dict['bill'] = row['number']
+                        summary_dict['title'] = row['title']
                         
-                        output_l.append(dic_i)
+                        subjects = get_congress_data(get_root_url(row['url']) + '/subjects', api_key=api_key)['subjects']
+                        
+                        related_bills = get_congress_data(get_root_url(row['url']) + '/relatedbills', api_key=api_key)
+                        
+                        # dic_i = {'metadata': metadata,
+                        # 'text' : text,
+                        # 'summary': summary,
+                        # 'subjects': subjects,
+                        # 'related_bills': related_bills
+                        # }
+                        summary_list.append(summary_dict)
+                        text_list.append(text_dict)
+                        list_cosponsors.extend(cosponsor_l)
+                        output_l.append({'bill number': bill_number,
+                                        'policy_area': policy_area, 'latest_action': latest_action,
+                                        'cosponsor_D_perc': cosponsor_D, 'cosponsor_R_perc': cosponsor_R})
                         #TODO: optional, extract information from this. Otherwise 
                         
                         #TODO: extract: amendments, committees, cosponsors(!), titles                        
-                
+                        
                 except Exception as e:
                         print(e)
+                        
+        df_ouput_cosponsors = pd.concat(list_cosponsors)
+        
+        output_df = pd.DataFrame.from_records(output_l)
+        text_df = pd.DataFrame.from_records(text_list)
                         
 else:
         import urllib.request
